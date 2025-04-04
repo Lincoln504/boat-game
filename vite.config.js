e4fa57bd9e0ee46edc004602fef52d9fae797471
Lincoln Deen <lincolnadeen@gmail.com>
2025-04-04 01:24:23 -0700

    very basic import working, little shitty boat added. has boundary mesh to detect collision with shore, a water mesh, and a ground mesh.

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,18 @@
 //vite.config.js - Vite config: server, build, options.
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    open: true
+  },
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      'three': 'three'
+    }
+  }
+});
