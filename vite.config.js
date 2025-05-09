31d305bac357f4a859e950939238c2536dde555d
Lincoln Deen <lincolnadeen@gmail.com>
2025-05-08 19:07:37 -0700

    pat seein and base rm three add 4

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: '/l17-texture-mapping-lk911/', // Set base path for GitHub Pages
     build: {
         outDir: 'dist', // Output directory for build files
         emptyOutDir: true, // Clean the output directory before building
-        rollupOptions: {
-            external: ['three'],
-            output: {
-                globals: {
-                    three: 'THREE'
-                }
-            }
-        }
-
-
+        // Removed rollupOptions for externalizing three
     },
     plugins: [react()],
 })
