5364a357b0162939d7898af02eee3c5cc983163f
Lincoln Deen <lincolnadeen@gmail.com>
2025-05-08 18:56:46 -0700

    pat seein and base rm three add

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-
     build: {
         outDir: 'dist', // Output directory for build files
-        emptyOutDir: true // Clean the output directory before building
+        emptyOutDir: true, // Clean the output directory before building
+        rollupOptions: {
+            external: ['three'],
+            output: {
+                globals: {
+                    three: 'THREE'
+                }
+            }
+        }
+
+
     },
     plugins: [react()],
 })
